3a4722e754a4db7f7128bd1dbaa690dbbd5fcc76
cleitonsilva14 <cleiton18ads@gmail.com>
2026-01-18 12:43:36 -0300

    Arquivo Extra.kt

---
+++ src/Extra.kt
@@ -0,0 +1,19 @@
+
+
+fun main(){
+
+//    var counter = 10
+//
+//    for (c in 'A'..'Z') {
+//        println("$c = $counter")
+//        counter++
+//    }
+
+
+    var i = 1_000_000
+    var l = 1_000_000_000_000
+    println("$i, $l")
+    println(i::class)
+    println(l::class)
+
+}
