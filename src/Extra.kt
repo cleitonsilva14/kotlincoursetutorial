c5d7699e2b6a3931d70e5743c292900bd17fe67e
cleitonsilva14 <cleiton18ads@gmail.com>
2026-01-18 13:03:30 -0300

    Arquivo Extra.kt

--- src/Extra.kt
+++ src/Extra.kt
@@ -1,4 +1,9 @@
+import com.sun.jdi.Value
+import java.time.LocalDateTime
 
+fun repeatChar(){
+    println("-".repeat(45))
+}
 
 fun main(){
 
@@ -16,7 +21,44 @@ fun main(){
     println(i::class)
     println(l::class)
 
+    repeatChar()
     println(Int.MIN_VALUE)
     println(Double.MAX_VALUE)
+    repeatChar()
 
+    repeatChar()
+
+    println("kotlin" === "KOTLIN")
+    println("KOTLIN" === "KOTLIN")
+    repeatChar()
+
+    var n:Long = 5
+    println("${++n}")
+
+    repeatChar()
+    println(invert(51))
+    println(invert(5.0))
+    repeatChar()
+
+
+    makeSomething("Cookie", "Kotlin")
+    makeSomething("Request", "Kotlin", LocalDateTime.now().plusWeeks(-2), false)
+    repeatChar()
+
+}
+
+
+fun invert(number:Int): Int {
+    return number * -1
+}
+
+fun invert(number:Double): Double = number * -1
+
+fun makeSomething(
+    name: String,
+    value: String? = null,
+    expires: LocalDateTime? = LocalDateTime.now().plusMinutes(30),
+    secure: Boolean? = true
+){
+    println("Name: $name, Value: $value, Expires: $expires, Secure: $secure")
 }
